662bc5a6ffacde7d0811358a1556c9e5c3e3521a
Maung <maung.alive@gmail.com>
2023-12-30 23:44:42 +0630

    NavBar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { getNews } from './api/getNews'
+import NavBar from './components/NavBar';
 
 
 export default function App() {
@@ -11,11 +12,12 @@ export default function App() {
   useEffect(() => {
     getNews(query, sortBy, setNews);
   }, [])
-  
+
   return (
-    <div className='text-3xl'>
-      <h1>News API</h1>
-      {news.map(i => { return <h2 key={i.title}>{i.title}</h2> })}
-    </div>
+    <>
+
+    <NavBar />
+    
+    </>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaBars, FaSearch } from 'react-icons/fa';
+
+export default function NavBar() {
+  return (
+    <div className='sticky top-0 left-0 w-full py-3 px-6 bg-inherit flex justify-around items-center'>
+        <h3 className='text-4xl font-medium'>N News</h3>
+        <div className="flex items-center relative">
+            <input type="text" name="search" id="search" className='w-full border-2 border-gray-950 rounded-xl pl-2 pr-8 py-2' />
+            <FaSearch className='absolute top-1/2 -translate-y-1/2 right-4' />
+        </div>
+        <FaBars className='text-3xl lg:hidden' />
+    </div>
+  )
+}
