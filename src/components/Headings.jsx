529772fd13b10cbcedc1c77d0d69551f5e94a541
Maung <maung.alive@gmail.com>
2023-12-31 10:53:25 +0630

    Responsive code

--- src/components/Headings.jsx
+++ src/components/Headings.jsx
@@ -3,13 +3,13 @@ import Slider from './Slider'
 
 export default function Headings({ headlines }) {
   return (
-    <div className='flex justify-center gap-x-6 items-center mt-10 w-full h-[500px]'>
+    <div className='block md:flex justify-center gap-x-6 items-center mt-10 w-full md:h-[500px]'>
 
-        <Slider data={headlines} className="h-full w-[750px]" />
+        <Slider data={headlines} className="h-[400px] md:h-full md:w-[600px] lg:w-[750px] mb-5 md:mb-0" />
 
-        <div className="w-[400px] h-full bg-white rounded-xl overflow-y-auto">
+        <div className="w-full md:w-[400px] h-[400px] md:h-full bg-white rounded-xl overflow-y-auto">
           {headlines.map(data => 
-            <h3 className="text-xl font-medium py-3 px-2 hover:border-l-4 cursor-pointer border-black">{data?.title}</h3>  
+            <h3 className="text-llg font-medium py-3 px-2 hover:border-l-4 cursor-pointer border-black">{data?.title}</h3>  
           )}
         </div>
 
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -6,9 +6,9 @@ const Item = ({data}) => {
   return (
     <div className="w-full h-full rounded-2xl overflow-hidden relative bg-gray-400">
       <img src={data?.urlToImage} alt={data?.title} className="w-full h-full object-cover" />
-      <div className="absolute bottom-0 left-0 p-2 pb-10 w-full bg-[rgba(255,255,255,0.4)] rounded-t-xl">
-        <p className="text-3xl font-medium hover:text-white">{data?.title}</p>
-        <p className="text-lg py-2">{data?.description}</p>
+      <div className="absolute bottom-0 left-0 p-2 pb-10 w-full bg-[rgba(200,200,200,0.4)] rounded-t-xl">
+        <p className="text-xl md:text-2xl lg:text-3xl font-medium hover:text-white">{data?.title}</p>
+        <p className="text-sm md:text-md lg:text-lg py-2">{data?.description}</p>
       </div>
     </div>
   )
@@ -32,10 +32,10 @@ export default function Slider({data, className}) {
   return (
     <div className={`relative ${className}`}>
       <Item data={data[current]} />
-      <div className='absolute top-1/2 left-0 -translate-y-1/2 rounded-e-xl text-white hover:text-black duration-200 hover:bg-[rgba(200,200,200,0.4)] p-2' onClick={prevSlide}>
+      <div className='absolute top-10 md:top-1/2 left-0 -translate-y-1/2 rounded-e-xl text-white hover:text-black duration-200 hover:bg-[rgba(200,200,200,0.4)] p-2' onClick={prevSlide}>
         <FaChevronLeft className='text-3xl' />
       </div>
-      <div className='absolute top-1/2 right-0 -translate-y-1/2 rounded-s-xl text-white hover:text-black duration-200 hover:bg-[rgba(200,200,200,0.4)] p-2' onClick={nextSlide}>
+      <div className='absolute top-10 md:top-1/2 right-0 -translate-y-1/2 rounded-s-xl text-white hover:text-black duration-200 hover:bg-[rgba(200,200,200,0.4)] p-2' onClick={nextSlide}>
         <FaChevronRight className='text-3xl' />
       </div>
       <div className="flex gap-x-1 relative bottom-2 -translate-y-full justify-center w-full items-center">
